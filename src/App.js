83dff9aec9c56efc03f70c50065b28ba649700b1
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-04-14 10:46:17 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import PaymentDetail from './components/PaymentDetail/PaymentDetail';
+function App() {
+  return (
+  <>
+<PaymentDetail />
+  </>
+  );
+}
+
+export default App;
